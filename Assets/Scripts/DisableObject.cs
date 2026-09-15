b0350af5f0bf72968ab7aea09835c46cfd715fa9
delta030737-pixel <delta030737@gmail.com>
2026-09-15 15:10:52 +0700

    text

---
+++ Assets/Scripts/DisableObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DisableObject : MonoBehaviour
+{
+    public GameObject Obj;
+    public float activeTime;
+
+    void Update()
+    {
+        if (Obj.active == true)
+        {
+            StartCoroutine(DisableObj());
+        }
+    }
+
+    IEnumerator DisableObj()
+    {
+        yield return new WaitForSeconds(activeTime);
+        Obj.SetActive(false);
+    }
+}
